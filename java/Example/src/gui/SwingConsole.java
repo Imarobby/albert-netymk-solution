f9afbf532ff8d0bb9d7e4db23142916cc17188c0
Albert Netymk <albertnetymk@gmail.com>
2011-09-12 20:12:47 +0200

    SwingConsole has been synchronized all the places.

--- java/Example/src/gui/SwingConsole.java
+++ java/Example/src/gui/SwingConsole.java
@@ -1,25 +1,30 @@
 package gui;
 
-import java.util.List;
-import java.util.ArrayList;
-
-import javax.swing.JFrame;
-import javax.swing.UIManager;
-import javax.swing.SwingUtilities;
+import java.awt.Toolkit;
 import java.lang.reflect.InvocationTargetException;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 public class SwingConsole {
-	public static void run(final int width, final int height, final Class<? extends JFrame> type) {
+	public static void run(final int width, final int height,
+			final Class<? extends JFrame> type) {
 		run(width, height, UIManager.getSystemLookAndFeelClassName(), type);
 	}
 
-	public static void run(final int width, final int height, final String lookAndFeel,
-		   	final Class<? extends JFrame> type) {
+	public static void run(final Class<? extends JFrame> type) {
+		run(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit
+				.getDefaultToolkit().getScreenSize().height, type);
+	}
+
+	public static void run(final int width, final int height,
+			final String lookAndFeel, final Class<? extends JFrame> type) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					UIManager.setLookAndFeel(lookAndFeel);
-				} catch(Exception e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				try {
--- java/Test/src/Test.java
+++ java/Test/src/Test.java
@@ -3,14 +3,25 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
-public class Test {
+public class Test extends JFrame {
+	public Test() {
+		add(new MyPanel());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// setSize(500,500);
+		setVisible(true);
+	}
 	public static void main(String[] args) {
-		Random rand = new Random(47);
-		int i = 0, j = 0;
-		while(i <= 100){
-		   	j = rand.nextInt(3);
-			i += j;
-			System.out.println(j);
-		}
+		new Test();
+	}
+}
+class MyPanel extends JPanel {
+	public MyPanel() {
+		setVisible(true);
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		setBackground(Color.red);
 	}
 }
