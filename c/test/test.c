b9190463cb7b884a4cefe583d161e8a4a10cb0a6
Albert Netymk <albertnetymk@gmail.com>
2011-10-06 10:51:32 +0200

    latex is add to workspace.

--- c/test/test.c
+++ c/test/test.c
@@ -2,43 +2,11 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-typedef struct char_print_struct {
-	char character;
-	int count;
-} char_print;
-
-void* print_char(void* argv)
-{
-	int i;
-	char_print *p = argv;
-	for(i=0; i<p->count; ++i) {
-		fputc(p->character, stdout);
-	}
-	fputc('\n', stdout);
-
-	return NULL;
-}
-
 int main() {
-	pthread_t thread1;
-	pthread_t thread2;
-	char_print *thread1_argv, *thread2_argv;
-	thread1_argv = malloc(sizeof(char_print));
-	thread2_argv = malloc(sizeof(char_print));
-
-	thread1_argv->character = 'x';
-	thread1_argv->count = 20;
-	pthread_create(&thread1, NULL, &print_char, thread1_argv);
-
-	thread2_argv->character = 'x';
-	thread2_argv->count = 10;
-	pthread_create(&thread2, NULL, &print_char, thread2_argv);
-
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
-
-	free(thread1_argv);
-	free(thread2_argv);
-
+	printf("%c\n", 34);
+	printf("%c\n", 3);
+	printf("%c\n", 17);
+	printf("%c\n", 11);
+	printf("%c\n", 82);
 	return 0;
 }
--- java/Test/src/Test.java
+++ java/Test/src/Test.java
@@ -1,27 +1,22 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 import java.util.*;
 
-public class Test extends JFrame {
-	public Test() {
-		add(new MyPanel());
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// setSize(500,500);
-		setVisible(true);
-	}
-	public static void main(String[] args) {
-		new Test();
-	}
-}
-class MyPanel extends JPanel {
-	public MyPanel() {
-		setVisible(true);
-	}
+public class Test {
+	private static Comparator<Card> comparator = new Comparator<Card>() {
+		public int compare(Card o1, Card o2) {
+			return o1.rank - o1.rank;
+		}
+	};
+	private static TreeSet<Card> sortedHand = new TreeSet<Card>(comparator);
 
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-
-		setBackground(Color.red);
+	public static int getUtility(List<Card> cards) {
+		sortedHand.clear();
+		sortedHand.addAll(cards);
+		Card previous = null;
+		for(Card current : cards) {
+			if(previous != null) {
+			}
+			previous = current;
+		}
+		return 0;
 	}
 }
